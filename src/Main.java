e4911613d775f5bf8df9eef63db68c2bd60fa687
[firstname <[valid-email]>
2023-02-25 13:20:37 +0100

    Ejercicio 8. El ejercio esat contenida en la calse persona y los objetos creados en clase principal

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,45 @@
 public class Main {
     public static void main(String[] args) {
 
+        //EJERCICIO NUMERO 8
+        //Crear un objeto persona en el main.
+        Persona persona = new Persona();
+        persona.setNombre("Edwin");
+        persona.setEdad(45);
+        persona.setTelefono(0031.638497646);
+        //Utiliza los gets y sets para darle valores a las propiedades edad, nombre y telefono, por último muéstralas por consola.
+        System.out.println("Persona numero 1");
+        System.out.println("Esta persona se llama " + persona.getNombre() +
+                "\n\t Tiene "  + persona.getEdad() + " años de edad. " +
+                "\n\t Y, su número de teléfono es: " + persona.getTelefono());
+
+        System.out.println();
+
+        Persona persona1 = new Persona();
+        persona1.setNombre("Carlos");
+        persona1.setEdad(20);
+        persona1.setTelefono(0032.6384);
+
+        System.out.println("Persona dos:");
+        System.out.println("Esta persona se llama " + persona1.getNombre() +
+                "\n\t Tiene "  + persona1.getEdad() + " años de edad. " +
+                "\n\t Y, su número de teléfono es: " + persona1.getTelefono());
+
+        System.out.println();
+        Persona persona2 = new Persona();
+        persona2.setNombre("Mario");
+        persona2.setEdad(70);
+        persona2.setTelefono(6453627.0);
+
+        System.out.println("Persona tres:");
+        System.out.println("Esta persona se llama " + persona2.getNombre() +
+                "\n\t Tiene "  + persona2.getEdad() + " años de edad. " +
+                "\n\t Y, su número de teléfono es: " + persona2.getTelefono());
+
+
+
+    }
+
         //Ejercicio Tres =
 
 //        int resultado = sumadorDeNumeros(2, 3,4);
@@ -11,58 +50,87 @@ public class Main {
 //        System.out.println(miChoche);
 
         //Ejercio 4
-         int numero = -10;
-         if(numero > 0){
-             System.out.println( numero + " Este número es positivo.");
-         } else {
-             System.out.println( numero + " Este numero es negativo.");
-         }
 
-         int numeroWhile = 0;
-
-         while (numeroWhile < 3){
-             System.out.println(numeroWhile);
-             numeroWhile++;
-         }
-
-        int doWhile = 3;
-
-
-        do{
-            System.out.println(doWhile);
-            doWhile ++;
-        }while (doWhile < 3);
-
-        int numeroFor [] = {1, 2, 3};
-
-      for (int i = 0; i <= numeroFor.length; i++) {
-            System.out.println(numeroFor[i]);
-      }
-
-      String estacion = "Primavera" ;
-
-      switch (estacion){
-          case "Primavera":
-              System.out.println("Las flores llegan!!!");
-              break;
-          case "Verano":
-                  System.out.println("Pongase su bikini!!!");
-                  break;
-          case "Otoño":
-              System.out.println("Pongase una casaca contra lluvias y vientos!!!");
-              break;
-          case"Invierno":
-              System.out.println("Ponagse a invernar!!!");
-              break;
-          default:
-              System.out.println("Usted no esta viviendo en ninguna de las estaciones");
-      }
-
-
-    }
-
-    //Ejercio 3.
-    public static int sumadorDeNumeros(int a, int b, int c){
-        return a + b +c;
-    }
+//        //Condicional if
+//        int numero = -10;
+//        if (numero > 0) {
+//            System.out.println(numero + " Este número es positivo.");
+//        } else {
+//            System.out.println(numero + " Este numero es negativo.");
+//        }
+//
+//        //bucle while
+//        System.out.println("Bucle while");
+//
+//        int numeroWhile = 0;
+//
+//        while (numeroWhile < 3) {
+//            System.out.println(numeroWhile);
+//            numeroWhile++;
+//        }
+//
+//        // bucle do while
+//        System.out.println("Bucle do while");
+//
+//        int doWhile = 2;
+//
+//
+//        do {
+//            System.out.println(doWhile);
+//            doWhile++;
+//        } while (doWhile < 3);
+//
+//        //bucle for
+//        System.out.println("bucle for");
+//        int lista[] = new int[5];//declaration and instantiation
+//        lista[0] = 10;//initialization
+//        lista[1] = 20;
+//        lista[2] = 70;
+//        lista[3] = 40;
+//        lista[4] = 50;
+//
+//        for (int i = 0; i < lista.length; i++)//length is the property of array
+//            System.out.println(lista[i]);
+//
+//        System.out.println("Array dos");
+//
+//   int[] list = {1, 2, 3};
+//        for (int k = 0; k < list.length; k++) {
+//            System.out.println(list[k]);
+//        }
+//
+//
+//// bucle switch
+//      String estacion = "Primavera" ;
+//
+//      switch (estacion){
+//          case "Primavera":
+//              System.out.println("Las flores llegan!!!");
+//              break;
+//          case "Verano":
+//                  System.out.println("Pongase su bikini!!!");
+//                  break;
+//          case "Otoño":
+//              System.out.println("Pongase una casaca contra lluvias y vientos!!!");
+//              break;
+//          case"Invierno":
+//              System.out.println("Ponagse a invernar!!!");
+//              break;
+//          default:
+//              System.out.println("Usted no esta viviendo en ninguna de las estaciones");
+//      }
+//
+//
+//    //  array
+//
+//        int numerosArray[] = new int[10];
+//        System.out.println(numerosArray[6]);
+//
+//
+//    }
+//
+//   // Ejercio 3.
+//    public static int sumadorDeNumeros(int a, int b, int c){
+//        return a + b +c;
+//    }
 }
---
+++ src/Persona.java
@@ -0,0 +1,41 @@
+
+//Crear variables las privadas edad, nombre y telefono de la clase Persona.
+public class Persona {
+     private int edad;
+     private String nombre;
+     private double telefono;
+
+
+
+     //Crear gets y sets de cada propiedad.
+
+    //Setters
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(double telefono) {
+        this.telefono = telefono;
+    }
+
+
+    // Getters
+
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getTelefono() {
+        return telefono;
+    }
+}
